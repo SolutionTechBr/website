248a73e609791648ea7127e27194ccbd8fb47d5e
gustavo <gustavo@solutiontech.dev>
2023-09-05 15:39:54 -0300

    Add BigTech

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Servico from "./Servico/Servico";
 import HomePage from "./HomePage/HomePage";
 import Contato from "./Contato/Contato";
 import About from "./About/About";
-import Error from "./Error/404";    
+import Error from "./Error/404";  
+import BigTech from "./About/Bigtech";
 
-export { Servico, HomePage, Contato, About, Error };
+export { Servico, HomePage, Contato, About, Error, BigTech };
